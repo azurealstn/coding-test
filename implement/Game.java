49e25c70e43547584a660f1c2cb9a1abbccc98a3
azurealstn <55525868+azurealstn@users.noreply.github.com>
2020-10-01 01:11:46 +0900

    Create Game.java

---
+++ implement/Game.java
@@ -0,0 +1,81 @@
+package com.minsu.algorithm;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static int n, m, x, y, direction;
+
+    //방문한 위치를 저장하기 위한 맵을 생성하여 0으로 초기화
+    public static int[][] d = new int[50][50];
+
+    //전체 맵 정보
+    public static int[][] arr = new int[50][50];
+
+    // 북, 동, 남, 서 방향 정의
+    public static int dx[] = {-1, 0, 1, 0};
+    public static int dy[] = {0, 1, 0, -1};
+
+    //왼쪽으로 회전
+    public static void turn_left() {
+        direction -= 1; //direction: 바라보고 있는 방향
+        if (direction == -1) direction = 3;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        // N, M을 공백을 기준으로 입력받기
+        n = sc.nextInt();
+        m = sc.nextInt();
+
+        // 현재 캐릭터의 X좌표, Y좌표, 방향 입력받기
+        x = sc.nextInt();
+        y = sc.nextInt();
+        direction = sc.nextInt();
+        d[x][y] = 1; //현재 좌표 방문 처리
+
+        //전체 맵 입력받기
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                arr[i][j] = sc.nextInt();
+            }
+        }
+
+        //시뮬레이션 시작
+        int cnt = 1; //방문한 칸의 수
+        int turn_time = 0; //회전 횟수
+        while (true) {
+            //왼쪽 회전
+            turn_left();
+            int nx = x + dx[direction];
+            int ny = y + dy[direction];
+            //회전 이후 정면에 가보지 않은 칸이 존재하는 경우 이동
+            if (d[nx][ny] == 0 && arr[nx][ny] == 0) {
+                d[nx][ny] = 1;
+                x = nx;
+                y = ny;
+                cnt += 1;
+                turn_time = 0;
+                continue;
+            }
+            //회전 이후 정면에 가보지 않은 칸이 없거나 바다인 경우
+            else turn_time += 1; //회전을 해야함
+            //네 방향 모두 갈 수 없는 경우
+            if (turn_time == 4) { //다 돌았을 경우
+                nx = x - dx[direction];
+                ny = y - dy[direction];
+                //뒤로 갈 수 있다면 이동
+                if (arr[nx][ny] == 0) {
+                    x = nx;
+                    y = ny;
+                }
+                //뒤도 바다로 막혀있을 경우
+                else break;
+                turn_time = 0; //0으로 다시 초기화
+            }
+        }
+        System.out.println(cnt);
+    }
+}
+
