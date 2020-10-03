5c7c1a128d72bcd3ccb8e094aa6274eb83fe65e1
azurealstn <55525868+azurealstn@users.noreply.github.com>
2020-10-03 22:04:05 +0900

    Create Insertion.java

---
+++ Sort/Insertion.java
@@ -0,0 +1,23 @@
+package com.minsu.algorithm;
+
+public class Main {
+    public static void main(String[] args) {
+        int[] arr = {7, 5, 9, 0, 3, 1, 6, 2, 4, 8}; //0부터9까지 랜덤 배치
+
+        for (int i = 1; i < arr.length; i++) {
+            //인덱스 i부터 1까지 감소하며 반복하는 문법
+            for (int j = i; j > 0; j--) {
+                if (arr[j] < arr[j-1]) {
+                    int temp = arr[j];
+                    arr[j] = arr[j-1];
+                    arr[j-1] = temp;
+                }
+                else break;
+            }
+        }
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+    }
+}
+
